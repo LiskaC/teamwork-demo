9f568f088f6f0a87d880521b631b93f1b5077e33
Liska Crofts <38409100+LiskaC@users.noreply.github.com>
2024-04-03 11:52:12 +0200

    FE-3: Add a basic navbar with four elements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { LoginView } from './login/Login';
+import { Navbar } from './navbar/Navbar';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>ReDI Teamwork Demo</h1>
       <LoginView/>
     </div>
---
+++ frontend/src/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/**
+ * A navigational UI component
+ */
+export const Navbar = () => {
+  return (
+    <ul>
+      <li><a href='www.google.com'>Library</a></li>
+      <li><a href='www.google.com'>Profile</a></li>
+      <li><a href='www.google.com'>Process</a></li>
+      <li><a href='www.google.com'>Frogs</a></li>
+    </ul>
+  )
+}
